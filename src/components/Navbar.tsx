b3d4cf94b45fb3d09da3f00d1f24697f23cf2405
Knowledge-JO <megamindtheincredible@gmail.com>
2024-08-09 23:20:19 +0100

    fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
+import { TonConnectButton } from "@tonconnect/ui-react";
+
 export default function Navbar() {
   return (
     <div className="text-white flex justify-between items-center px-5 md:px-10 lg:px-16 xl:px-48 py-10 ">
       <div className="text-2xl">
         <a href="/">Yield</a>
       </div>
-      <button className="bg-blue-900 p-3 rounded-2xl">connect wallet</button>
+      {/* <button className="bg-blue-900 p-3 rounded-2xl">connect wallet</button> */}
+      <TonConnectButton />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TonConnectUIProvider manifestUrl="https://rose-gothic-goose-655.mypinata.cloud/ipfs/QmPDEh6qUyb67aCpox7T2anjBQjNyyVRCEbnQ4NHHXDkoi">
+      <App />
+    </TonConnectUIProvider>
+  </StrictMode>
+);
